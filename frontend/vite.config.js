15199af87b1ea2caf9c15990149767b2d0a4f196
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2025-12-30 22:56:01 +0530

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://rma-ashen.vercel.app',
         changeOrigin: true,
       }
     }
